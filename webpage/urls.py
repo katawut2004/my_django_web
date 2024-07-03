81be98eabc19e87fc9698218a44af38ca133aaab
katawut2004 <mr.katawut2004@gmail.com>
2024-07-03 09:53:39 +0700

    add web templates

--- webpage/urls.py
+++ webpage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.indexPage),
+    path("index/", views.indexPage),
     path("about/", views.aboutUs),
     path("contact/", views.contactUs),
 ]
