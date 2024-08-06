67f60fa5cad5164b0cc780c8b3723d64f0b6d2a3
katawut2004 <mr.katawut2004@gmail.com>
2024-08-06 11:34:54 +0700

    update static

--- my_web/settings.py
+++ my_web/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [BASE_DIR / 'statics', ]
+STATICFILES_DIRS = [BASE_DIR / 'static', ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
