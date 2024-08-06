94b6ff4ec9888a2de3e1b931592d1baeca152c3a
katawut2004 <mr.katawut2004@gmail.com>
2024-08-06 11:46:21 +0700

    upversel

--- webpage/urls.py
+++ webpage/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.indexPage, name="index"),
     path("about/", views.aboutUs, name="about"),
-    path("contact/", views.contactUs),
+    path("contact/", views.contactUs, name="contact"),
 ]
