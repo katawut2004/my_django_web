b9c9993ee746f077e804eb02881ccfa1b6c66fdd
katawut2004 <mr.katawut2004@gmail.com>
2024-08-07 09:29:21 +0700

    updatefortest

--- webpage/urls.py
+++ webpage/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.indexPage, name="index"),
     path("about/", views.aboutUs, name="about"),
     path("contact/", views.contactUs, name="contact"),
+    path('for/', views.forPage, name="for-page"),
 ]
